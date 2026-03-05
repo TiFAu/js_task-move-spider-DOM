5fba79c604cfd1e787f6af1a5959633b8eb007be
TiFAu <turchin.anton2@gmail.com>
2026-03-06 00:08:27 +0200

    added first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+// eslint-disable-next-line no-shadow
+document.addEventListener('click', (event) => {
+  const coordX = event.clientX;
+  const coordY = event.clientY;
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const leftBorderField = wall.offsetLeft + wall.clientLeft + spider.width / 2;
+  const topBorderField = wall.offsetTop + wall.clientTop + spider.height / 2;
+  const fieldWidth = wall.clientWidth - spider.width;
+  const fieldHeight = wall.clientHeight - spider.height;
+
+  if (
+    coordX > leftBorderField &&
+    coordX < leftBorderField + fieldWidth &&
+    coordY > topBorderField &&
+    coordY < topBorderField + fieldHeight
+  ) {
+    spider.style.left = `${coordX - leftBorderField}px`;
+    spider.style.top = `${coordY - topBorderField}px`;
+
+    // console.log(spider.style.left, spider.style.top);
+  }
 });
